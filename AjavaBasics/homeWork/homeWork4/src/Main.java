f0ebff662ed430294f895a882b1b0c904f34315f
Gavrilov-S <3672010@mail.ru>
2024-11-08 10:02:36 +0300

    finished the job

--- AjavaBasics/homeWork/homeWork4/src/Main.java
+++ AjavaBasics/homeWork/homeWork4/src/Main.java
@@ -42,34 +42,67 @@ public class Main {
         //Task 4
         System.out.println("Task 4");
         int years = rd.nextInt(0, 30);
-        if (years > 2 && years < 6) System.out.printf("Если возраст человека равен %d, то ему нужно ходить в детский сад!", years);
-        if (years > 7 && years < 17) System.out.printf("Если возраст человека равен %d, то ему нужно ходить в школу!", years);
-        if (years >= 18 && years <= 24) System.out.printf("Если возраст человека равен %d, то его место в университете!", years);
-        if (years > 24 ) System.out.printf("Если возраст человека равен %d, то ему пора ходить на работу!", years);
-        System.out.println("\n");
+        if (years > 2 && years < 6) {
+            System.out.printf("Если возраст человека равен %d, то ему нужно ходить в детский сад!\n", years);
+        } else if (years >= 6 && years < 17) {
+            System.out.printf("Если возраст человека равен %d, то ему нужно ходить в школу!\n", years);
+        } else if (years >= 18 && years <= 24) {
+            System.out.printf("Если возраст человека равен %d, то его место в университете!\n", years);
+        } else if (years > 24) {
+            System.out.printf("Если возраст человека равен %d, то ему пора ходить на работу!\n", years);
+        }
+        System.out.println();
 
         //Task 5
         System.out.println("Task 5");
-        int yearsСhild = rd.nextInt(0, 15);
-        if (yearsСhild < 5) System.out.printf("Если возраст ребенка равен %d, то ему " +
-                "нельзя кататься на аттракционе!", yearsСhild);
-        if (yearsСhild > 5 && yearsСhild < 14) System.out.printf("Если возраст ребенка равен %d, то ему " +
-                "может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя!", yearsСhild);
-        if (yearsСhild > 14 ) System.out.printf("Если возраст ребенка равен %d, то он " +
-                "может кататься без сопровождения взрослого!", yearsСhild);
+        int yearsChild = rd.nextInt(0, 15);
+        if (yearsChild < 5) {
+            System.out.printf("Если возраст ребенка равен %d, то ему нельзя кататься на аттракционе!\n", yearsChild);
+        } else if (yearsChild >= 5 && yearsChild < 14) {
+            System.out.printf("Если возраст ребенка равен %d, то ему можно кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя!\n", yearsChild);
+        } else {
+            System.out.printf("Если возраст ребенка равен %d, то он может кататься без сопровождения взрослого!\n", yearsChild);
+        }
+        System.out.println();
 
         //Task 6
         System.out.println("\n");
         System.out.println("Task 6");
 
         int numberOfPeople = rd.nextInt(0, 103);
-        if (numberOfPeople < 60) {
+        if (numberOfPeople < 60 && numberOfPeople > 0) {
             System.out.printf("В вагоне человек: %d, есть сидячие места!", numberOfPeople);
         } else if ( numberOfPeople > 60 && numberOfPeople < 102){
             System.out.printf("В вагоне человек: %d, нет сидячие мест!", numberOfPeople);
         } else {
             System.out.printf("В вагоне человек: %d, в вагоне нет мест!", numberOfPeople);
         }
+
+        //Task 7
+        System.out.println("\n");
+        System.out.println("Task 7");
+        int one = rd.nextInt(-100, 101);
+        int two = rd.nextInt(-100, 101);
+        int three = rd.nextInt(-10, 101);
+        if (one >= two) {
+            if (three >= one) {
+                System.out.println("Сгенерированные числа: " + one + ", " + two + ", " + three);
+                System.out.printf("из трех чисел бо́льшее three = %d", three);
+            } else {
+                System.out.println("Сгенерированные числа: " + one + ", " + two + ", " + three);
+                System.out.printf("из трех чисел бо́льшее one = %d", one);
+            }
+        } else {
+            if (two >= three) {
+                System.out.println("Сгенерированные числа: " + one + ", " + two + ", " + three);
+                System.out.printf("из трех чисел бо́льшее two = %d", two);
+            }else {
+                System.out.println("Сгенерированные числа: " + one + ", " + two + ", " + three);
+                System.out.printf("из трех чисел бо́льшее three = %d", three);
+            }
+        }
+
+
 
 
 
