c4c315648f1ae08157c8301fa4b13538fdcbbd87
Gavrilov-S <3672010@mail.ru>
2024-11-23 13:45:23 +0300

    end work string

--- AjavaBasics/homeWork/homeWork8Strings/src/Main.java
+++ AjavaBasics/homeWork/homeWork8Strings/src/Main.java
@@ -1,11 +1,29 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         //Task 1
+        System.out.println("\n\nTask 1");
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println(fullName);
+        System.out.printf("Ф. И. О. сотрудника - %s", fullName);
 
+        //Task 2
+        System.out.println("\n\nTask 2");
+        System.out.printf("Данные Ф. И. О. сотрудника для заполнения отчета - %s", fullName.toUpperCase(Locale.ROOT));
+
+        //Task 3
+        System.out.println("\n\nTask 3");
+        fullName = "Иванов Семён Семёнович";
+        String text = "";
+        for (String i : fullName.split("")) {
+            if (i.equals("ё")) {
+                i = "e";
+            }
+            text += i;
+        }
+        System.out.printf("Данные Ф. И. О. сотрудника - %s", text);
     }
 }
