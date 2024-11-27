c39cfeb74f7810f6432d3d5572e2e86bebfc91db
Gavrilov-S <3672010@mail.ru>
2024-11-27 15:00:02 +0300

    end work

--- AjavaBasics/homeWork/HW10-ObjectsAndClasses/src/Book.java
+++ AjavaBasics/homeWork/HW10-ObjectsAndClasses/src/Book.java
@@ -1,3 +1,32 @@
 public class Book {
+    String titleBook;
+    Author author;
+    int yearPublication;
+
+    public Book(String titleBook, Author author, int yearPublication) {
+        this.titleBook = titleBook;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getTitleBook() {
+        return this.titleBook;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getYearPublication() {
+        return this.yearPublication;
+    }
+
+    public void setTitleBook(String titleBook) {
+        this.titleBook = titleBook;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 
 }
