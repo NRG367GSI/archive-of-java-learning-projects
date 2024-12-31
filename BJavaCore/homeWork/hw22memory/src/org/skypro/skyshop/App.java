a493fc8b971031dfe2cc02eff608e1ef1c196212
Gavrilov-S <3672010@mail.ru>
2024-12-31 16:46:20 +0300

    Adding a product to the shopping cart

--- BJavaCore/homeWork/hw22memory/src/org/skypro/skyshop/App.java
+++ BJavaCore/homeWork/hw22memory/src/org/skypro/skyshop/App.java
@@ -1,7 +1,25 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.Product;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product apple = new Product("Apple", 100);
+        Product banana = new Product("Banana", 150);
+        Product orange = new Product("Orange", 120);
+        Product mango = new Product("Mango", 200);
+        Product pineapple = new Product("Pineapple", 250);
+        Product watermelon = new Product("Watermelon", 300);
+        Product kiwi = new Product("Kiwi", 80);
+
+        ProductBasket basket = new ProductBasket(5);
+
+        //Добавление продукта в корзину.
+        basket.addProduct(apple);
+        basket.addProduct(banana);
+        basket.addProduct(orange);
+        basket.addProduct(mango);
+        basket.addProduct(pineapple);
     }
 }
